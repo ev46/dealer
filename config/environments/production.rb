dafd02993eb8ec7348264d3fd1b850b319e6dff2
ev46 <ev46@cornell.edu>
2014-12-18 22:30:55 -0500

    added paperclip info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,10 +87,12 @@ Cars::Application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   # paperclip and s3 storage config data
-  config.paperclip_defaults = {
+ config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 
